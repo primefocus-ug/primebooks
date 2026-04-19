06e7d21eb482e191dd8b93461eb31800eb871f0b
primefocus-ug <primefocusug@gmail.com>
2026-04-19 19:35:59 +0300

    feat:firebase

--- push_notifications/urls.py
+++ push_notifications/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from . import views
-from .push_debug_view import push_debug   # ✅ FIX
 
 app_name = 'push_notifications'
 
 urlpatterns = [
     path('subscribe/', views.save_subscription, name='subscribe'),
     path('vapid-key/', views.get_vapid_public_key, name='vapid_key'),
-    path('debug/', push_debug, name='push_debug'),
     path('my-preferences/', views.my_push_preferences, name='my_preferences'),
     path('manage/<int:user_id>/', views.manage_user_push_preferences, name='manage_user'),
 ]
